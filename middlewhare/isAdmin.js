ff4d1d2fb8670b397dd7ac259afaac3c3f15f590
Chernomur <chernomur777@gmail.com>
2020-08-19 15:05:17 +0300

    fix

--- middlewhare/isAdmin.js
+++ middlewhare/isAdmin.js
@@ -1,12 +1,12 @@
 module.exports = async (req, res, next) => {
   try {
     if (req.user.role !== "admin") {
-      return res.status(403).json({ message: 'You have no permissions to see that' });
+      throw { code: 403, message: "You have no permissions to see that" };
     }
 
     next();
   } catch (e) {
-    console.error(e)
+    console.error(e);
     res.sendStatus(500);
   }
-}
+};
